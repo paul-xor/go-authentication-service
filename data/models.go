4a0a1d5de5cc9bdf2e08c64618403033ca3f509b
Pasha <psmolov@nautilusdigital.com>
2024-07-09 13:20:46 -0400

    wip: start with Repository pattern for data package

--- data/models.go
+++ data/models.go
@@ -14,22 +14,32 @@ const dbTimeout = time.Second * 3
 
 var db *sql.DB
 
-// New is the function used to create an instance of the data package. It returns the type
-// Model, which embeds all the types we want to be available to our application.
-func New(dbPool *sql.DB) Models {
-	db = dbPool
+type PostgresPepository struct {
+	Conn *sql.DB
+}
 
-	return Models{
-		User: User{},
+func NewPostgresRepository(db *sql.DB) *PostgresPepository {
+	return &PostgresPepository{
+		Conn: db,
 	}
 }
+
+// New is the function used to create an instance of the data package. It returns the type
+// Model, which embeds all the types we want to be available to our application.
+// func New(dbPool *sql.DB) Models {
+// 	db = dbPool
+
+// 	return Models{
+// 		User: User{},
+// 	}
+// }
 
 // Models is the type for this package. Note that any model that is included as a member
 // in this type is available to us throughout the application, anywhere that the
 // app variable is used, provided that the model is also added in the New function.
-type Models struct {
-	User User
-}
+// type Models struct {
+// 	User User
+// }
 
 // User is the structure which holds one user from the database.
 type User struct {
